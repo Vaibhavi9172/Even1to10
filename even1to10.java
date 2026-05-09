e2cfa2c1f18e67bb49fa93c9450cbb376c558e72
Vaibhavi Narayan Hande <vaibhavihande13@gmail.com>
2026-05-09 16:00:28 +0530

    Add files via upload

---
+++ even1to10.java
@@ -0,0 +1,13 @@
+package forloops;
+
+public class even1to10 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++) {
+			//System.out.println(i++);
+			if(i%2==0)
+				System.out.println(i);
+	}
+
+}
+}
